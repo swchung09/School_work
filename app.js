a417c561f029b660375902e8b6f32de433ce458c
swchung09 <swchung16@gmail.com>
2025-07-15 23:50:56 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,17 @@
-var current_background = 0;
-var background1 = document.getElementById("background1");
-var background2 = document.getElementById("background2");
-var background3 = document.getElementById("background3");
-var background4 = document.getElementById("background4");
-var background5 = document.getElementById("background5");
-var background6 = document.getElementById("background6");
-var background7 = document.getElementById("background7");
+let current_background = 0;
+let background = [];
+for (let i = 0; i < 7; i++){
+    background[i] = document.getElementById(`background${i+1}`);
+}
 function next_background(){
     current_background++;
-    background1.style.display = "none";
-    background2.style.display = "none";
-    background3.style.display = "none";
-    background4.style.display = "none";
-    background5.style.display = "none";
-    background6.style.display = "none";
-    background7.style.display = "none";
-    switch (current_background){
-        case 1:
-            background1.style.display = "flex";
-            break;
-        case 2:
-            background2.style.display = "flex";
-            break;
-        case 3:
-            background3.style.display = "flex";
-            break;
-        case 4:
-            background4.style.display = "flex";
-            break;
-        case 5:
-            background5.style.display = "flex";
-            break;
-        case 6:
-            background6.style.display = "flex";
-            break;
-        case 7:
-            background7.style.display = "flex";
-            break;
-        default:
-            current_background = 1;
-            background1.style.display = "flex";
-            break;
+    for (let i = 0; i < 7; i++){
+        background[i].style.display = 'none';
     }
+    if (background.length < current_background){
+        current_background = 1;
+    }
+    background[current_background-1].style.display = 'flex';
 }
 next_background();
 document.addEventListener("keydown", function(event) {
