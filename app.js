d06f4c09341b5eb9270fce1529063d8d7b08f1a9
swchung09 <swchung16@gmail.com>
2025-07-19 20:33:32 +0900

    시작 화면 추가, 배경에 교실_의자 부분 추가

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 let current_background = 0, x = 0, y = 0
-let background = [];
+let background = [], relative = [], main_character, placeholder, button;
 for (let i = 0; i < 7; i++){
     background[i] = document.getElementById(`background${i+1}`);
+    relative[i] = document.getElementById(`background-r${i+1}`);
 }
+main_character = document.getElementById('main_character');
+placeholder = document.getElementById('placeholder')
+button = document.getElementById('button')
 function next_background(){
     current_background++;
     for (let i = 0; i < 7; i++){
         background[i].style.display = 'none';
+        if (relative[i]){
+            relative[i].style.display = 'none';
+        }
     }
     if (background.length < current_background){
         current_background = 1;
     }
     background[current_background-1].style.display = 'flex';
+    if (relative[current_background-1]){
+        relative[current_background-1].style.display = 'flex';
+    }
 }
+
 function key_event_handler(event) {
     next_background();
 }
+
 function mouse_event_handler(event) {
-    var main_character = document.getElementById('main_character');
     var rect = background[current_background - 1].getBoundingClientRect();
     var charrect = main_character.getBoundingClientRect();
     let x = event.clientX;
@@ -31,6 +42,16 @@ function mouse_event_handler(event) {
     main_character.style.left = x + 'px';
     main_character.style.top = y + 'px';
 }
-next_background();
-document.addEventListener("keydown", key_event_handler);
-document.addEventListener("mousemove", mouse_event_handler);
+
+main_character.style.display = 'none';
+placeholder.style.width = background[0].naturalWidth+'px';
+placeholder.style.height = background[0].naturalHeight+'px';
+
+button.addEventListener('click', function(event){
+    placeholder.style.display = 'none';
+    button.style.display = 'none';
+    main_character.style.display = 'flex';
+    document.addEventListener("keydown", key_event_handler);
+    document.addEventListener("mousemove", mouse_event_handler);
+    next_background();
+})
