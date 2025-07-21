3623c3a0ff1c4713ebeae7ebc6de83e344c1fe64
swchung09 <swchung16@gmail.com>
2025-07-21 22:10:59 +0900

    코드 수정(노션 참고)

--- app.js
+++ app.js
@@ -23,12 +23,7 @@ function next_background(){
         relative[current_background-1].style.display = 'flex';
     }
 }
-
-function key_event_handler(event) {
-    next_background();
-}
-
-function mouse_event_handler(event) {
+async function mouse_event_handler(event) {
     var rect = background[current_background - 1].getBoundingClientRect();
     var charrect = main_character.getBoundingClientRect();
     let x = event.clientX;
@@ -41,17 +36,70 @@ function mouse_event_handler(event) {
     if (y > rect.bottom - charrect.height) y = rect.bottom - charrect.height;
     main_character.style.left = x + 'px';
     main_character.style.top = y + 'px';
+    x = (event.clientX - rect.left) / rect.width;
+    y = (event.clientY - rect.top) / rect.height;
+    switch (current_background){
+        case 1:
+            if (0.93 < x && 0.71 < y){
+                next_background();
+            }
+            break;
+        case 2:
+            if (x < 0.01 && 0.5 < y){
+                console.log(x, y, 's');
+            }
+            break;
+        case 4:
+            if (0.83 < x && 0.74 < y){
+                next_background();
+            }else if (0.6 > x){
+                console.log(x);
+                x *= background[current_background - 1].naturalWidth;
+                y *= background[current_background - 1].naturalHeight;
+                if (y > (800 / 1649) * x + 780){
+                    let died = document.getElementById('died');
+                    document.removeEventListener('mousemove', mouse_event_handler);
+                    document.removeEventListener('keydown', key_event_handler);
+                    main_character.style.display = 'none';
+                    for (let i = 0; i < 7; i++) {
+                        background[i].style.display = 'none';
+                        if (relative[i]) {
+                            relative[i].style.display = 'none';
+                        }
+                    }
+                    died.style.display = 'flex';
+                    await new Promise((resolve) => {
+                        const handler = (event) => {
+                            var rect = died.getBoundingClientRect();
+                            x = (event.clientX - rect.left) / rect.width;
+                            y = (event.clientY - rect.top) / rect.height;
+                            if (0.57 < x && x < 0.86 && 0.66 < y && y < 0.82){
+                                document.removeEventListener('click', handler);
+                                died.style.display = 'none';
+                                placeholder.style.display = 'flex';
+                                button.style.display = 'flex';
+                                current_background = 1000;
+                            }
+                        }
+                        document.addEventListener('click', handler)
+                    });
+                }
+            }
+            break;
+    }
 }
-
+function key_event_handler(event) {
+    next_background();
+}
 main_character.style.display = 'none';
 placeholder.style.width = background[0].naturalWidth+'px';
 placeholder.style.height = background[0].naturalHeight+'px';
-
 button.addEventListener('click', function(event){
     placeholder.style.display = 'none';
     button.style.display = 'none';
     main_character.style.display = 'flex';
     document.addEventListener("keydown", key_event_handler);
     document.addEventListener("mousemove", mouse_event_handler);
+    next_background();
     next_background();
 })
