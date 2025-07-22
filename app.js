d4e5b6a62130373bb9ff45bbe18e26d9f4a9f3b6
mossikim <mossikim12@gmail.com>
2025-07-23 00:23:42 +0900

    Update app.js
    히트박스 구현 (코드 추가 및 수정)
     - 추가적으로 검수 및 테스트 필요 (*테스트 진행 안함)
     - 주기값 등 상황에 맞게 코드 변경 또는 치환 필요
    
    기본적으로 마우스 포인터를 기준으로 확인하도록 작성함. 이후에 캐릭터에 맞춰서 코드 수정 또는 재작성 필요.
    코드 주석 추가 및 사용된 함수 (특히 주기) 관련해서 추가적으로 찾아보고 이해도 향상 필요.
    
    혹시 모를 에러 등을 고려해 분기에 작성함. 검수 후 메인에 업로드 필요.

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 let current_background = 0, x = 0, y = 0
 let background = [], relative = [], main_character, placeholder, button;
+
+//추가된 부분 (히트박스 핸들링 위함)
+//--------------------------------------------------------------------------------------
+let mouse_pos = {x:0, y:0}; // 히트박스 처리 위한 마우스 포인터 위치값 저장
+let check_intv = null; // 히트박스 처리 함수 호출 주기
+const obstacle = []; // [n번째 배경][m번째 장애물] = {x1: val, y1: val, x2: val, y2: val} 과 같이 배경-장애물 부분은 배열로, 왼쪽위-오른쪽아래 좌표는 dictionary로 저장
+//--------------------------------------------------------------------------------------
+
 for (let i = 0; i < 7; i++){
     background[i] = document.getElementById(`background${i+1}`);
     relative[i] = document.getElementById(`background-r${i+1}`);
@@ -28,6 +36,13 @@ async function mouse_event_handler(event) {
     var charrect = main_character.getBoundingClientRect();
     let x = event.clientX;
     let y = event.clientY;
+    
+//추가된 부분 (히트박스 핸들링 위함)
+//--------------------------------------------------------------------------------------
+    mouse_pos.x = event.clientX; //x, y값 mouse_pos로 받음, 받은 값은 checker() 를 통해서 히트 체킹
+    mouse_pos.y = event.clientY;
+//--------------------------------------------------------------------------------------    
+    
     x -= charrect.width / 2;
     y -= charrect.height;
     if (x < rect.left) x = rect.left;
@@ -91,6 +106,22 @@ async function mouse_event_handler(event) {
 function key_event_handler(event) {
     next_background();
 }
+
+//추가된 부분 (히트박스 핸들링 위함)
+//--------------------------------------------------------------------------------------
+function checker(){
+    const current_obs = obstacle[current_background - 1]; //현재 배경의 장애물 정보 받아옴
+    if(!current_obs || current_obs.length === 0){ //만약 장애물이 없다면 그냥 반환 (최적화를 위함이며, 함수 바깥에 무언가 저장, 전달하는 방법 통해서 추가 최적화도 가능할 듯?)
+        return;
+    }
+    for(const obs of current_obs){ //각 장애물마다 확인 (장애물 정보 순회)
+        if( mouse_pos.x > obs.x1 && mouse_pos.y > obs.y1 && mouse_pos.x < obs.x2 && mouse_pos.y < obs.y2 ) { //마우스 포인터의 위치가 장애물 (왼쪽 위, 오른쪽 아래로 직사각형 히트박스 구현) 안에 있는지 확인
+            return; //현재는 반환이지만 이후 died 배경 전환코드로 치환 또는 수정 예정
+        }
+    }
+}
+//--------------------------------------------------------------------------------------
+
 main_character.style.display = 'none';
 placeholder.style.width = background[0].naturalWidth+'px';
 placeholder.style.height = background[0].naturalHeight+'px';
@@ -100,5 +131,12 @@ button.addEventListener('click', function(event){
     main_character.style.display = 'flex';
     document.addEventListener("keydown", key_event_handler);
     document.addEventListener("mousemove", mouse_event_handler);
+
+//추가된 부분 (히트박스 핸들링 위함)
+//--------------------------------------------------------------------------------------
+    if(check_intv) clearInterval(check_intv); //interval 활용해서 100ms 주기로 확인 (값 변경할듯? 100ms는 너무 긴거같음, interval 관련 함수 등은 좀 더 찾아봐야함 (이해 부족))
+    check_intv = setInterval(checker, 100);
+//--------------------------------------------------------------------------------------
+    
     next_background();
 })
