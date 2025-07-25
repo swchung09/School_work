b14e490e25d69753ea7234dc8d91439ba9656ace
swchung09 <swchung16@gmail.com>
2025-07-25 22:30:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -322,7 +322,7 @@ async function checker(event){
     var x, y;
     x = (event.clientX - rect.left) / rect.width;
     y = (event.clientY - rect.top) / rect.height;
-    const current_obs = obstacle[current_background]; //현재 배경의 장애물 정보 받아옴
+    const current_obs = obstacle[current_background-1]; //현재 배경의 장애물 정보 받아옴
     if(!current_obs || current_obs.length === 0){ //만약 장애물이 없다면 그냥 반환 (최적화를 위함이며, 함수 바깥에 무언가 저장, 전달하는 방법 통해서 추가 최적화도 가능할 듯?)
         return;
     }
